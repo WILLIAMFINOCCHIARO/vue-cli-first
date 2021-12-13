d7bff1625871e989250a1e19d2c69ab2ca6ee897
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-12-13 13:25:51 +0100

    complete

---
+++ src/components/hero.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="hero"></div>
+</template>
+
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+
+<style scoped>
+#hero {
+  background-color: red;
+}
+</style>
---
+++ src/components/title.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+<script>
+
+
+export default {
+name:"title",
+    
+};
+
+</script>
+
+
+<style scoped>
+#title {
+  color:red;
+}
+</style>
